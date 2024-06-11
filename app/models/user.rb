f6c2b6131e972b4f48d5a6b73fe5d2f1b6164d7a
michiru <michiru.shigemitsu@gmail.com>
2024-06-12 06:22:24 +0900

    update:エラーメッセージ追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
-  validates :name, presence: true, length: { maximum: 30 }
-  validates :email, presence: true, length: { maximum: 255 }, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+ validates :name, presence: true, length: { maximum: 30 } 
+  # errors.add(:name, "名前を入力してください")
+  #validates :name, presence: {message:"名前を入力してください"}
+  #validates :email, presence: true, length: { maximum: 255 }, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :email, presence: true, length: { maximum: 255 }, uniqueness: {message:"メールアドレスはすでに使用されています"}, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_validation { email.downcase! }
   has_secure_password
-  validates :password, length: { minimum: 6 }
+  validates :password, length: { minimum: 6,message: "パスワードは6文字以上で入力してください" }
 end
